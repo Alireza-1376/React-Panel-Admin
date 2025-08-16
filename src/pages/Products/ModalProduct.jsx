27fde11178fa3dd2c70c3de511859c464e9ce34c
alireza <alirezahabibi1376118@gmail.com>
2025-08-16 17:03:53 +0330

    panel-admin part#41

--- src/pages/Products/ModalProduct.jsx
+++ src/pages/Products/ModalProduct.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Icon from "../../layouts/sidebar/Icons";
 import { FastField, Formik, Form, ErrorMessage } from "formik";
-import { object, string } from "yup";
+import { number, object, string } from "yup";
 import { useEffect, useState } from "react";
 import { get } from "../../services/httpRequest";
 import { PulseLoader } from "react-spinners";
@@ -29,7 +29,7 @@ const onSubmit = (values) => {
 }
 const validationSchema = object({
   parent_ids: string().required("لطفا دسته ی والد را انتخاب کنبد"),
-  category_ids: string().required("لطفا دسته ی محصول را انتخاب کنبد").matches(/^-?\d+\.?\d*$/, "آیدی محصول را وارد کنید"),
+  category_ids: string().required("لطفا دسته ی محصول را انتخاب کنبد"),
   title: string().required('لطفا عنوان محصول را وارد کنید'),
   price: string().required("لطفا قیمت محصول را وارد کنید").matches(/^-?\d+\.?\d*$/, "لطفا عدد وارد کنید"),
 })
@@ -41,9 +41,10 @@ const ModalProduct = () => {
   const token = JSON.parse(localStorage.getItem('token'));
   const [categoryParents, setCategoryParents] = useState([]);
   const [categoryChildrens, setCategoryChildrens] = useState([]);
-  const [loading ,setLoading] =useState(false)
+  const [selectChildren, setSelectChildren] = useState([]);
+  const [loading, setLoading] = useState(false)
 
-  async function getCategoriesParents() {
+  async function getCategoriesParents(e, form) {
     try {
       const response = await get("/admin/categories", "", { Authorization: `Bearer ${token}` })
       setCategoryParents(response.data.data)
@@ -74,207 +75,248 @@ const ModalProduct = () => {
     }
   }
 
-  
+
+  function selectChildrenCategory(e ,form) {
+    if (e.target.value != "") {
+      const filter = categoryChildrens.filter((item) => {
+        return item.id == e.target.value;
+      })
+      setSelectChildren((prev) => {
+        if (prev.findIndex((item) => { return item.id == e.target.value }) == -1) {
+          const newData =[...prev, filter[0]]
+          const selectId =newData.map((item)=>{
+            return item.id; 
+          })
+          form.setFieldValue("category_ids" ,selectId.join("-"))
+          return newData ; 
+        } else {
+          return [...prev]
+        }
+      })
+    }
+
+    
+
+  }
+
+  function deleteSelectChildren(id , form) {
+    const filter = selectChildren.filter((item) => {
+      return item.id != id;
+    })
+    setSelectChildren(filter);
+    const filterId =filter.map((item)=>{
+      return item.id
+    })
+    form.setFieldValue("category_ids" ,filterId.join("-"))
+  }
+
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={onSubmit}
       validationSchema={validationSchema}
     >
-      <Form className="text-center space-y-4 mt-4 p-4">
-        <h2 className="text-center text-2xl pt-6 mt-10">افزودن محصول جدید</h2>
+      {formik => {
+        console.log(formik.values)
+       return <Form className="text-center space-y-4 mt-4 p-4">
+          <h2 className="text-center text-2xl pt-6 mt-10">افزودن محصول جدید</h2>
 
-        <div className="flex flex-col justify-center">
-          {categoryParents.length > 0 ?
-            <div className="flex flex-1 justify-center">
-              <span className="bg-blue-300/50 border border-gray-400 py-2 w-1/4 md:w-28 px-4">
-                دسته والد
-              </span>
-              <FastField name="parent_ids">
-                {props => {
-                  return (
-                    <select onChange={(e) => getCategoryChildrens(e)} className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400">
-                      <option value="">انتخاب دسته والد</option>
-                      {categoryParents.map((item) => {
-                        return <option key={item.id} value={item.id}>{item.title}</option>
-                      })}
-                    </select>
-                  )
-                }}
-              </FastField>
-            </div>
-            : null}
-          <ErrorMessage name="parent_ids">
-            {error => {
-              return <span className="text-sm text-red-500">{error}</span>
-            }}
-          </ErrorMessage>
-        </div>
-
-        <div className="flex flex-col gap-1">
-
-          {categoryChildrens.length > 0 ? 
-          <>
           <div className="flex flex-col justify-center">
-            <div className="flex flex-1 justify-center">
-              <span className="bg-blue-300/50 border border-gray-400 py-2 w-1/4 md:w-28 px-4">
-                دسته اصلی
-              </span>
-              <FastField name="category_ids" >
-                {props => {
-                  return (
-                    <select className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400">
-                      <option value="">دسته ی مورد نظر را انتخاب کنید</option>
-                      {categoryChildrens.map((item) => {
-                        return <option value={item.id}>{item.title}</option>
-                      })}
-                    </select>
-                  )
-                }}
-              </FastField>
-            </div>
-
-            <ErrorMessage name="category_ids">
+            {categoryParents.length > 0 ?
+              <div className="flex flex-1 justify-center">
+                <span className="bg-blue-300/50 border border-gray-400 py-2 w-1/4 md:w-28 px-4">
+                  دسته والد
+                </span>
+                <FastField name="parent_ids">
+                  {props => {
+                    return (
+                      <select onChange={(e) => { getCategoryChildrens(e); props.form.setFieldValue("parent_ids", e.target.value) }} className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400">
+                        <option value="">انتخاب دسته والد</option>
+                        {categoryParents.map((item) => {
+                          return <option key={item.id} value={item.id}>{item.title}</option>
+                        })}
+                      </select>
+                    )
+                  }}
+                </FastField>
+              </div>
+              : null}
+            <ErrorMessage name="parent_ids">
               {error => {
                 return <span className="text-sm text-red-500">{error}</span>
               }}
             </ErrorMessage>
           </div>
+
+          <div className="flex flex-col gap-1">
+
+            {categoryChildrens.length > 0 ?
+              <>
+                <div className="flex flex-col justify-center">
+                  <div className="flex flex-1 justify-center">
+                    <span className="bg-blue-300/50 border border-gray-400 py-2 w-1/4 md:w-28 px-4">
+                      دسته اصلی
+                    </span>
+                    <FastField name="category_ids" >
+                      {props => {
+                        return (
+                          <select onChange={(e) => { selectChildrenCategory(e ,props.form) }} className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400">
+                            <option value="">دسته ی مورد نظر را انتخاب کنید</option>
+                            {categoryChildrens.map((item) => {
+                              return <option key={item.id} value={item.id}>{item.title}</option>
+                            })}
+                          </select>
+                        )
+                      }}
+                    </FastField>
+                  </div>
+
+                  <ErrorMessage name="category_ids">
+                    {error => {
+                      return <span className="text-sm text-red-500">{error}</span>
+                    }}
+                  </ErrorMessage>
+                </div>
+                <div className="flex justify-center">
+                  <div className="flex justify-start gap-1 w-full md:w-5/6 lg:w-3/4 md:pr-[10%] lg:pr-[8.5%]">
+                    {selectChildren.map((item) => {
+                      return (
+                        <span key={item.id} className="flex items-center bg-green-200 py-1 px-4 rounded-full">
+                          <button type="button" onClick={() => { deleteSelectChildren(item.id ,formik) }}>
+                            <Icon name="xMark" />
+                          </button>
+                          <span>{item.title}</span>
+                        </span>
+                      )
+                    })}
+
+                  </div>
+                </div>
+              </>
+              : loading ? <div className="text-center"><PulseLoader size={20} color="purple" /></div> : null}
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="flex justify-center">
+              <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
+                عنوان
+              </span>
+              <FastField name="title"
+                placeholder="عنوان محصول"
+                type="text"
+                className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
+              />
+            </div>
+            <ErrorMessage name="title">
+              {error => {
+                return <span className="text-sm text-red-500">{error}</span>
+              }}
+            </ErrorMessage>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <div className="flex justify-center">
+              <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
+                قیمت
+              </span>
+              <FastField name="price"
+                placeholder="قیمت محصول"
+                type="text"
+                className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
+              />
+            </div>
+            <ErrorMessage name="price">
+              {error => {
+                return <span className="text-sm text-red-500">{error}</span>
+              }}
+            </ErrorMessage>
+          </div>
+
           <div className="flex justify-center">
-            <div className="flex justify-start gap-1 w-full md:w-5/6 lg:w-3/4 md:pr-[10%] lg:pr-[8.5%]">
-              <span className="flex items-center bg-green-200 py-1 px-4 rounded-full">
-                <Icon name="xMark" />
-                <span>دسته فلان</span>
-              </span>
-              <span className="flex items-center bg-green-200 py-1 px-4 rounded-full">
-                <Icon name="xMark" />
-                <span>دسته فلان</span>
-              </span>
+            <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
+              وزن
+            </span>
+            <FastField name="weight"
+              placeholder="وزن محصول (کیلوگرم)"
+              type="text"
+              className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
+            />
+          </div>
+
+          <div className="flex justify-center">
+            <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
+              برند
+            </span>
+            <div className=" w-3/4 flex md:w-1/2 border border-gray-400 bg-white">
+              <FastField name=" brand_id"
+                placeholder="قسمتی از نام برند را انتخاب کنید"
+                type="text"
+                className="focus:outline-none flex-1 px-2"
+              />
+              <button className="bg-blue-300/50 border p-2 px-4">+</button>
             </div>
           </div>
-          </>
-          : loading ?  <div className="text-center"><PulseLoader size={20} color="purple" /></div>  : null}
-        </div>
 
-        <div className="flex flex-col justify-center">
-          <div className="flex justify-center">
-            <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
-              عنوان
-            </span>
-            <FastField name="title"
-              placeholder="عنوان محصول"
-              type="text"
-              className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
-            />
-          </div>
-          <ErrorMessage name="title">
-            {error => {
-              return <span className="text-sm text-red-500">{error}</span>
-            }}
-          </ErrorMessage>
-        </div>
-
-        <div className="flex flex-col justify-center">
-          <div className="flex justify-center">
-            <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
-              قیمت
-            </span>
-            <FastField name="price"
-              placeholder="قیمت محصول"
-              type="text"
-              className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
-            />
-          </div>
-          <ErrorMessage name="price">
-            {error => {
-              return <span className="text-sm text-red-500">{error}</span>
-            }}
-          </ErrorMessage>
-        </div>
-
-        <div className="flex justify-center">
-          <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
-            وزن
-          </span>
-          <FastField name="weight"
-            placeholder="وزن محصول (کیلوگرم)"
-            type="text"
-            className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
-          />
-        </div>
-
-        <div className="flex justify-center">
-          <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
-            برند
-          </span>
-          <div className=" w-3/4 flex md:w-1/2 border border-gray-400 bg-white">
-            <FastField name=" brand_id"
-              placeholder="قسمتی از نام برند را انتخاب کنید"
-              type="text"
-              className="focus:outline-none flex-1 px-2"
-            />
-            <button className="bg-blue-300/50 border p-2 px-4">+</button>
-          </div>
-        </div>
-
-        <div>
-          <div className="flex justify-center">
-            <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
-              رنگ
-            </span>
-            <FastField name="color_ids"
-              placeholder="قسمتی از نام رنگ را انتخاب کنید"
-              type="text"
-              className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <div className="flex justify-center">
-            <span className="bg-blue-300/50 border border-gray-400 py-2 w-1/4 md:w-28 px-4">
-              گارانتی
-            </span>
-            <FastField name="guarantee_ids"
-              placeholder="قسمتی از نام گارانتی را وارد کنید"
-              type="text"
-              className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
-            />
-          </div>
-          <div className="flex justify-center">
-            <div className="flex justify-start gap-1 w-full md:w-5/6 lg:w-3/4 md:pr-[11.5%] lg:pr-[9%]">
-              <span className="flex items-center bg-green-200 py-1 px-4 rounded-full">
-                <Icon name="xMark" />
-                <span>گارانتی فلان</span>
+          <div>
+            <div className="flex justify-center">
+              <span className="bg-blue-300/50 border border-gray-400 w-1/4 md:w-28 py-2 px-4">
+                رنگ
               </span>
-              <span className="flex items-center bg-green-200 py-1 px-4 rounded-full">
-                <Icon name="xMark" />
-                <span>گارانتی فلان</span>
-              </span>
+              <FastField name="color_ids"
+                placeholder="قسمتی از نام رنگ را انتخاب کنید"
+                type="text"
+                className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
+              />
             </div>
           </div>
-        </div>
 
-        <div className="flex justify-center">
-          <span className="bg-blue-300/50 border flex justify-center items-center border-gray-400 w-1/4 md:w-28 py-2 px-4">
-            توضیحات
-          </span>
-          <FastField name="descriptions"
-            as="textarea"
-            placeholder="توضیحات"
-            rows={5}
-            type="text"
-            className="focus:outline-none w-3/4 md:w-1/2 border border-gray-400 p-2"
-          ></FastField>
-        </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex justify-center">
+              <span className="bg-blue-300/50 border border-gray-400 py-2 w-1/4 md:w-28 px-4">
+                گارانتی
+              </span>
+              <FastField name="guarantee_ids"
+                placeholder="قسمتی از نام گارانتی را وارد کنید"
+                type="text"
+                className="focus:outline-none p-2 w-3/4 md:w-1/2 border border-gray-400"
+              />
+            </div>
+            <div className="flex justify-center">
+              <div className="flex justify-start gap-1 w-full md:w-5/6 lg:w-3/4 md:pr-[11.5%] lg:pr-[9%]">
+                <span className="flex items-center bg-green-200 py-1 px-4 rounded-full">
+                  <Icon name="xMark" />
+                  <span>گارانتی فلان</span>
+                </span>
+                <span className="flex items-center bg-green-200 py-1 px-4 rounded-full">
+                  <Icon name="xMark" />
+                  <span>گارانتی فلان</span>
+                </span>
+              </div>
+            </div>
+          </div>
 
-        <div className="flex justify-center gap-4">
-          <button className="bg-blue-600 text-white px-10 py-2 rounded-md">
-            ذخیره
-          </button>
-          <button type="button" onClick={() => { navigate(-1) }} className="text-white bg-gray-600 px-8 rounded-md py-2">بازگشت</button>
-        </div>
-      </Form>
+          <div className="flex justify-center">
+            <span className="bg-blue-300/50 border flex justify-center items-center border-gray-400 w-1/4 md:w-28 py-2 px-4">
+              توضیحات
+            </span>
+            <FastField name="descriptions"
+              as="textarea"
+              placeholder="توضیحات"
+              rows={5}
+              type="text"
+              className="focus:outline-none w-3/4 md:w-1/2 border border-gray-400 p-2"
+            ></FastField>
+          </div>
+
+          <div className="flex justify-center gap-4">
+            <button className="bg-blue-600 text-white px-10 py-2 rounded-md">
+              ذخیره
+            </button>
+            <button type="button" onClick={() => { navigate(-1) }} className="text-white bg-gray-600 px-8 rounded-md py-2">بازگشت</button>
+          </div>
+        </Form>
+
+      }}
     </Formik>
 
   );
