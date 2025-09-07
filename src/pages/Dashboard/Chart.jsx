d4e85bc80d0baefdac7d485bb4af4221900874d5
alireza <alirezahabibi1376118@gmail.com>
2025-09-07 12:19:57 +0330

    panel-admin part#79

--- src/pages/Dashboard/Chart.jsx
+++ src/pages/Dashboard/Chart.jsx
@@ -1,65 +1,111 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactApexChart from "react-apexcharts";
+import { get } from "../../services/httpRequest";
+import moment from "moment-jalaali";
+import { PulseLoader } from "react-spinners";
+let month = [
+  "فروردین",
+  "اردیبهشت",
+  "خرداد",
+  "تیر",
+  "مرداد",
+  "شهریور",
+  "مهر",
+  "آبان",
+  "آذر",
+  "دی",
+  "بهمن",
+  "اسفند",
+]
 const Chart = () => {
-  const [state, setState] = useState({
-    series: [
-      {
-        name: "میلیون تومان",
-        data: [0, 20, 20, 60, 60, 120, 180, 120, 125, 105, 110, 170],
-      },
-    ],
-    options: {
-      chart: {
-        height: 300,
-        type: "line",
-        fontFamily: "BYekan",
-        zoom: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "straight",
-      },
-      title: {
-        text: "نمودار فروش سالیانه ",
-        align: "center",
-      },
-      grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5,
-        },
-      },
-      xaxis: {
-        categories: [
-          "فروردین",
-          "اردیبهشت",
-          "خرداد",
-          "تیر",
-          "مرداد",
-          "شهریور",
-          "مهر",
-          "آبان",
-          "آذر",
-          "دی",
-          "بهمن",
-          "اسفند",
-        ],
-        tickAmount: 12,
+  const [chartMonth, setChartMonth] = useState([]);
+  const [amountMonth, setAmountMonth] = useState([]);
+  const [loading , setLoading] = useState(false)
+
+  const options = {
+    chart: {
+      height: 300,
+      type: "line",
+      fontFamily: "BYekan",
+      zoom: { enabled: false },
+    },
+    dataLabels: { enabled: false },
+    stroke: { curve: "straight" },
+    title: { text: "نمودار فروش سالیانه", align: "center" },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5,
       },
     },
-  });
+    xaxis: {
+      categories: chartMonth,
+      tickAmount: 12,
+    },
+  };
+   const series = [
+    {
+      name: "میلیون تومان",
+      data: amountMonth,
+    }
+  ];
+
+  async function getThisYearOrders() {
+    setLoading(true)
+    const token = JSON.parse(localStorage.getItem("token"))
+    const response = await get("/admin/orders/this_year_orders", "", { Authorization: `Bearer ${token}` })
+    if (response.status == 200) {
+      setLoading(false)
+      let monthArr = [];
+      let thisMonth = moment().jMonth()
+      for (let i = 0; i < 12; i++) {
+        if (thisMonth == -1) thisMonth = 11
+        monthArr.push({ month: thisMonth, amount: 0 })
+        thisMonth--;
+      }
+
+      const orders = response.data.data;
+      for (let order of orders) {
+        const monthIndex = moment(order.pay_at).jMonth()
+        const monthArrIndex = monthArr.findIndex((item) => {
+          return item.month == monthIndex;
+        })
+        monthArr[monthArrIndex].amount = monthArr[monthArrIndex].amount + parseInt((order.pay_amount/1000000).toFixed(0))
+      }
+
+      const chartArray = monthArr.map((item) => {
+        return month[item.month]
+      }).reverse()
+
+      const amountArry = monthArr.map((item) => {
+        return item.amount
+      }).reverse()
+
+      setAmountMonth(amountArry)
+
+      setChartMonth(chartArray)
+
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getThisYearOrders()
+  }, [])
+
+
+
+
   return (
-    <ReactApexChart
-      options={state.options}
-      series={state.series}
+    <>
+    {loading ?<div><PulseLoader size={20} color="purple"/></div> : <ReactApexChart
+      options={options}
+      series={series}
       type="line"
       height={300}
       className="w-full"
-    />
+    />}
+    </>
   );
 };
 
